2c5e3f80a835a4e1d5a6e96bee467a56ea59cdd3
BRYAMMMMM <bperaltan@est.ups.edu.ec>
2025-11-14 22:42:10 -0500

    Agregar imports, ruta e implementación del kernel gaussiano

--- FiltroGaussiano/python/python_secuencial.py
+++ FiltroGaussiano/python/python_secuencial.py
@@ -1,2 +1,20 @@
+import time
+import math
+import tracemalloc
+import numpy as np
+from PIL import Image
 
-print("Hello, World!")
+RUTA_IMAGEN = "/home/bryam/Escritorio/Ups/Computacion_Paralela/Proyecto/resultados/4.jpg"
+
+def generar_kernel_gaussiano(tamano):
+    sigma = tamano / 3.0
+    centro = tamano // 2
+    kernel = np.zeros((tamano, tamano), dtype=float)
+    for i in range(tamano):
+        for j in range(tamano):
+            x = i - centro
+            y = j - centro
+            kernel[i, j] = math.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    suma = np.sum(kernel)
+    kernel /= suma
+    return kernel
